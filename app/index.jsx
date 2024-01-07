1fa02f807b777a6b830f544869a5b1af3ec523c5
lexikels <lexi_kelsall@me.com>
2024-01-07 14:48:37 +1100

    create repository

---
+++ app/index.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import * as ReactDom from "react-dom/client";
+import "./index.css";
+import Popular from "./components/Popular";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='light'>
+      <div className='container'>
+        <Popular />
+      </div>
+
+    </div>
+
+  )
+    
+  }
+}
+
+const rootElement = document.getElementById("app");
+const root = ReactDom.createRoot(rootElement);
+root.render(<App />);
