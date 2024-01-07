90ffef5e1639384c6cdd84f3d34a389b7adabdde
lexikels <lexi_kelsall@me.com>
2024-01-07 15:01:05 +1100

    fix navbar rendering

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,40 +1,17 @@
-import * as React from 'react'
+import * as React from "react";
 
 export default class Popular extends React.Component {
-    constructor(props) {
-        super(props);
+  render() {
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
-        this.state = {
-            selectedLanguage: 'All',
-        };
-
-        this.updateLanguage = this.updateLanguage.bind(this);
-    }
-    updateLanguage(selectedLanguage) {
-        this.setState({
-            selectedLanguage,
-        });
-
-    }
-
-    render () {
-        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-        
-        return (
-            <main>
-                <select>
-                    onChange={(e) => this.updateLanguage(e.target.value)}
-                    selected={this.state.selectedLanguage}
-
-                    {languages.map((language) => (
-                        <option key={language} value={language}>
-                            {language}
-                        </option>
-
-                    ))}
-                </select>
-                {JSON.stringfy(this.state, null, 2)}
-            </main>
-        );
-    }
+    return (
+      <select>
+        {languages.map((language) => (
+          <option key={language} value={language}>
+            {language}
+          </option>
+        ))}
+      </select>
+    );
+  }
 }
