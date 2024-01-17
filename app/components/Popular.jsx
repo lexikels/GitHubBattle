e1d54e754335f32a8e9c786b848f68ad276a3236
lexikels <lexi_kelsall@me.com>
2024-01-17 23:47:51 +1100

    Add popular nav bar

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,17 +1,83 @@
 import * as React from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
+import Table from "./Table";
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <select
+      onChange={(e) => onUpdateLanguage(e.target.value)}
+      selected={selected}
+    >
+      {languages.map((language) => (
+        <option key={language} value={language}>
+          {language}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+LanguagesNav.protoTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
 
 export default class Popular extends React.Component {
-  render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      selectedLanguage: "All",
+      repos: null,
+      error: null,
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+      error: null,
+    });
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) =>
+        this.setState({
+          repos,
+          error: null,
+        })
+      )
+      .catch((error) => {
+        console.warn("error fetching repos: ", error);
+
+        this.setState({
+          error: `there was an error fetching the repositories`,
+        });
+      });
+  }
+  render() {
+    const { selectedLanguage, repos, error } = this.state;
     return (
-      <select>
-        {languages.map((language) => (
-          <option key={language} value={language}>
-            {language}
-          </option>
-        ))}
-      </select>
+      <main className="stack main-stack animate-in">
+        <div class="split">
+          <h1>Popular</h1>
+          <LanguagesNav
+            selected={selectedLanguage}
+            onUpdateLanguage={this.updateLanguage}
+          />
+        </div>
+
+        {error && <p className="text-center error">{error}</p>}
+
+        {repos && <pre>{<Table repos={repos} />}</pre>}
+      </main>
     );
   }
 }
