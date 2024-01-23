d818e1dbe7cdc40b85cc235896dfbdf9e630c851
lexikels <lexi_kelsall@me.com>
2024-01-23 15:14:28 +1100

    created Battle input and preview

--- app/index.jsx
+++ app/index.jsx
@@ -2,19 +2,17 @@ import * as React from "react";
 import * as ReactDom from "react-dom/client";
 import "./index.css";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 class App extends React.Component {
   render() {
     return (
-      <div className='light'>
-      <div className='container'>
-        <Popular />
+      <div className="light">
+        <div className="container">
+          <Battle />
+        </div>
       </div>
-
-    </div>
-
-  )
-    
+    );
   }
 }
 
