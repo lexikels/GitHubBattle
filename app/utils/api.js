568777bc8fd3c79ace7224f21d757184d67389ff
lexikels <lexi_kelsall@me.com>
2024-01-23 16:15:16 +1100

    create results API

--- app/utils/api.js
+++ app/utils/api.js
@@ -13,3 +13,66 @@ export function fetchPopularRepos(language) {
       return data.items;
     });
 }
+
+function getErrorMsg(message, username) {
+  if (message === "Not Found") {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+
+      return profile;
+    });
+}
+
+function getRepos(username) {
+  return (
+    fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+      //get 100 repos
+      .then((res) => res.json())
+      .then((repos) => {
+        if (repos.message) {
+          throw new Error(getErrorMsg(repos.message, username));
+        }
+        return repos;
+      })
+  );
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, { stargazers_count }) => {
+    return count + stargazers_count;
+  }, 0);
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos);
+}
+
+function getUserData(player) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+}
+
+function sortPlayer(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    sortPlayer
+  );
+}
