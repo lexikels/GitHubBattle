ece8a641bf349cb2f4b6770a4ac74d6c75710689
lexikels <lexi_kelsall@me.com>
2024-02-20 13:43:13 +1100

    code sharing

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,46 +1,25 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
   display: "flex",
 };
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hovering: false,
-    };
-
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  mouseOver() {
-    this.setState({ hovering: true });
-  }
-
-  mouseOut() {
-    this.setState({ hovering: false });
-  }
-
-  render() {
-    const { hovering } = this.state;
-    const { children, element } = this.props;
-
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={container}
-      >
-        {hovering === true && element}
-        {children}
-      </div>
-    );
-  }
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
 
 Tooltip.propTypes = {
