f8acd7169305601bd9b557cffc141d2ea9d7c7b1
lexikels <lexi_kelsall@me.com>
2024-02-26 11:26:01 +1100

    added routing

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,27 @@
 import * as React from "react";
-import * as ReactDom from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="light">
-        <div className="container">
-          <Popular />
+      <Router>
+        <div className="light">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Popular />} />
+              <Route path="/battle" element={<Battle />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
 
 const rootElement = document.getElementById("app");
-const root = ReactDom.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
